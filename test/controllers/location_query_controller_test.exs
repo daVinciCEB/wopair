5bb1924fe3120293a3a8735032bf98e9902051a9
Coby Benveniste <Coby.Benveniste@gmail.com>
2017-07-30 20:39:28 +0300

    Updated location controller test, I left some old functions in there before

--- test/controllers/location_query_controller_test.exs
+++ test/controllers/location_query_controller_test.exs
@@ -1,13 +1,7 @@
 defmodule WorkoutDemo.LocationQueryControllerTest do
   use WorkoutDemo.ConnCase
 
-  @valid_attrs %{latitude: 76.5, longitude: 120.5, radius: 1000}
   @valid_user_attrs %{name: "Coby Benveniste", email: "coby.benveniste@gmail.com", password: "thisisapassword", description: "some content", location: %Geo.Point{}, latitude: 76.5, longitude: 120.5, radius: 1000}
-  @invalid_attrs %{}
-
-  setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
-  end
 
   setup %{conn: conn} do
     changeset = WorkoutDemo.User.registration_changeset(%WorkoutDemo.User{}, @valid_user_attrs)
@@ -25,14 +19,18 @@ defmodule WorkoutDemo.LocationQueryControllerTest do
     assert json_response(conn, 200)["search_results"]
   end
 
+  # setup %{conn: conn} do
+  #   {:ok, conn: put_req_header(conn, "accept", "application/json")}
+  # end
+
   # test "finds all users in a radius in meters when data is valid using input parameters", %{conn: conn} do
   #   conn = post conn, location_query_path(conn, :users_within_radius), search: @valid_attrs
   #   assert json_response(conn, 200)["search_results"]
   # end
 
-  test "returns errors when search parameters are invalid", %{conn: conn} do
-    conn = post conn, location_query_path(conn, :users_within_radius), search: @invalid_attrs
-    assert json_response(conn, 422)["errors"]
-  end
+  # test "returns errors when search parameters are invalid", %{conn: conn} do
+  #   conn = post conn, location_query_path(conn, :users_within_radius), search: @invalid_attrs
+  #   assert json_response(conn, 422)["errors"]
+  # end
 
 end
