567b9dbfd9c5cf8bb5e521d1cb0b4521d7600527
Coby Benveniste <Coby.Benveniste@gmail.com>
2017-08-01 21:22:30 +0300

    Refactored tests to verify user

--- test/controllers/location_query_controller_test.exs
+++ test/controllers/location_query_controller_test.exs
@@ -1,12 +1,14 @@
 defmodule WorkoutDemo.LocationQueryControllerTest do
   use WorkoutDemo.ConnCase
 
-  @valid_user_attrs %{name: "Coby Benveniste", email: "coby.benveniste@gmail.com", password: "thisisapassword", description: "some content", location: %Geo.Point{}, latitude: 76.5, longitude: 120.5, radius: 1000}
+  @valid_user_attrs %{name: "Coby Benveniste", email: "coby.benveniste@gmail.com", verified: true, password: "thisisapassword", description: "some content", location: %Geo.Point{}, latitude: 76.5, longitude: 120.5, radius: 1000}
 
   setup %{conn: conn} do
     changeset = WorkoutDemo.User.registration_changeset(%WorkoutDemo.User{}, @valid_user_attrs)
     user = Repo.insert!(changeset)
     session = WorkoutDemo.Session.create_changeset(%WorkoutDemo.Session{user_id: user.id}, %{}) |> Repo.insert!
+    verification_changeset = WorkoutDemo.User.verification_changeset(user, @valid_user_attrs)
+    Repo.update!(verification_changeset)
 
     conn = conn
     |> put_req_header("accept", "application/json")
