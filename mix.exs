72cf7c6a44597212640472ba23675cefc6a19297
Coby Benveniste <Coby.Benveniste@gmail.com>
2017-08-07 20:27:34 +0300

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule WorkoutDemo.Mixfile do
 
   def project do
     [app: :workout_demo,
-     version: "0.2.2",
+     version: "0.2.3",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
