3ebe1821b521dadf0919f8ffbc7c54bd0c84d8f1
Coby Benveniste <Coby.Benveniste@gmail.com>
2017-08-10 22:13:36 +0300

    Updated deployment script

--- deploy.exs
+++ deploy.exs
@@ -4,6 +4,8 @@ defmodule WorkoutDemo.DeployCallbacks do
   def before_mix_digest(env) do
     # mkdir prevents complains about this directory not existing
     bash("mkdir", ~w[-p priv/static], cd: env.build_dir)
+    bash("mix", ~w[ecto.reset], cd: env.build_dir)
+    # no need for npm calls as this has no npm
     # bash("npm", ~w[install], cd: env.build_dir)
     # bash("npm", ~w[run deploy], cd: env.build_dir)
   end
